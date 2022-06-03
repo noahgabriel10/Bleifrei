0a3438a5d6ceeeaa9f8cfe4e7d28cbda7a4edc73
noahgabriel10 <83354010+noahgabriel10@users.noreply.github.com>
2022-06-03 11:35:40 +0200

    Create forms.js

---
+++ assets/js/forms.js
@@ -0,0 +1,31 @@
+const submit = document.getElementById("spielenbtn")
+const vorname = document.getElementById("vorname")
+const nachname = document.getElementById("nachname")
+const email = document.getElementById("email")
+submit.disabled = true
+
+const validate = () => {
+    if (email.value == "" || vorname.value == "" || nachname.value == "") {
+        submit.disabled = true
+    } else {
+        submit.disabled = false
+    }
+}
+
+email.addEventListener("keyup", (event) => {
+    validate()
+})
+
+
+submit.addEventListener("click", async (event) => {
+    event.preventDefault()
+    const result = await databaseClient.insertInto("users", ["vorname","nachname","email"], [vorname.value, nachname.value, email.value])
+    if (result.error) {
+        alert("Datenbank Fehler: " + JSON.stringify(result.error, null, 2))
+    }
+    else {
+        // Weiterleitung auf die Game Page  
+        location.href = "./game.html"
+    }
+
+})
